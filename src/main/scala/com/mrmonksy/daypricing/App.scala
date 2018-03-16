9293b3246ac6a4e24247fe462e1f85b783034509
steven <crashoverride025@gmail.com>
2018-03-16 02:19:07 -0500

    Added health metrics and the path is temporarily not working

--- src/main/scala/com/mrmonksy/daypricing/App.scala
+++ src/main/scala/com/mrmonksy/daypricing/App.scala
@@ -1,33 +1,81 @@
 package com.mrmonksy.daypricing
 
 
+import java.io.File
+
 import akka.actor.ActorSystem
+import akka.actor.FSM.LogEntry
 import akka.event.Logging
+import akka.http.javadsl.server.RouteResult
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.MediaTypeNegotiator
+import akka.http.scaladsl.server.RouteResult.Rejected
+import akka.http.scaladsl.server.directives.DebuggingDirectives
 import akka.stream.ActorMaterializer
+import com.mrmonksy.daypricing.config.RatesCollection
+import com.mrmonksy.daypricing.datamodifiers.PriceDataUtility
+import com.typesafe.config.ConfigFactory
+import org.joda.time.format.ISODateTimeFormat
 import spray.json.{DefaultJsonProtocol, _}
 
 object MyJsonProtocol extends DefaultJsonProtocol {
-  implicit val healthFormat = jsonFormat1(HealthResponse)
+  implicit val healthFormat = jsonFormat3(HealthResponse)
+  implicit val priceFormat = jsonFormat1(PriceResponse)
 }
 
 /**
   * This is a quirk of akka-http. It has to be a trait to be able to isolate it's self from the server standup.
   */
 trait ServiceRoutes {
+  //ConfigFacotry defaults to application.json, however, I changed this because I had some issues when building the fat jar and it was overwritting the reference.conf.
+  // - Given time I would revert this change
+  lazy val fixedRates = RatesCollection(ConfigFactory.parseFile(new File(System.getProperty("config"))))
+  lazy val normalizedPrices = fixedRates.rates.flatMap(PriceDataUtility.from)
+
+  lazy val dataManager = new PriceDataManager(normalizedPrices)
+
+
+  //FIXME: Record keeping (could be better), we're going to lie aobut the number of calls to avoid a divide by zero
+  var numberOfCalls = 1L
+  var allTimeSpent = 0L
+
   val route =
-    path("healthcheck") {
-      (get & extract(_.request.headers)) { headers =>
-        val mediaNegotiator = new MediaTypeNegotiator(headers)
-        val result = HealthResponse("ok")
-        complete {
-          HttpResponse(entity = convertHealthResultToResponse(result, mediaNegotiator))
+    path("findPrice/between") {
+      path(Segment / Segment) { case (fromDate, toDate) => {
+        (get & extract(_.request.headers)) { headers =>
+
+
+          complete {
+            numberOfCalls += 1
+            val startTime = System.currentTimeMillis()
+
+            val mediaNegotiator = new MediaTypeNegotiator(headers)
+            val fullFrom = ISODateTimeFormat.dateTime().parseDateTime(fromDate)
+            val fullEnd = ISODateTimeFormat.dateTime().parseDateTime(toDate)
+
+            val result = dataManager.findPriceBetween(fullFrom, fullEnd)
+            val smallResponse = PriceResponse(result.map(_.price).getOrElse("unavailable").toString)
+
+            allTimeSpent += (System.currentTimeMillis() - startTime)
+
+            HttpResponse(entity = convertPriceResponse(smallResponse, mediaNegotiator))
+          }
         }
       }
-    }
+      }
+    } ~
+      path("healthcheck") {
+        (get & extract(_.request.headers)) { headers =>
+          val mediaNegotiator = new MediaTypeNegotiator(headers)
+          val result = HealthResponse("ok", numberOfCalls, allTimeSpent / numberOfCalls)
+          complete {
+            HttpResponse(entity = convertHealthResultToResponse(result, mediaNegotiator))
+          }
+        }
+      }
+
 
   /**
     * Converts the health result to a response, based on it's content type. Yes this is terribly ugly. But I'm new to akka-http and could be fixed in another iteration.
@@ -37,7 +85,7 @@ trait ServiceRoutes {
     * @param mediaTypeNegotiator A media negotiator that determines if a content type is acceptable.
     * @return Returns a route with a result.
     */
-  private def convertHealthResultToResponse(health: HealthResponse, mediaTypeNegotiator: MediaTypeNegotiator) = {
+  def convertHealthResultToResponse(health: HealthResponse, mediaTypeNegotiator: MediaTypeNegotiator) = {
     var response = ""
     var contentType: ContentType.NonBinary = ContentTypes.`application/json`
     if (mediaTypeNegotiator.isAccepted(MediaTypes.`application/json`)) {
@@ -47,9 +95,13 @@ trait ServiceRoutes {
 
     } else {
       val pureXml =
-        <status>
+        <response><status>
           {health.status}
-        </status>
+        </status> <totalCalls>
+          {health.totalCalls}
+        </totalCalls> <avgResponseTime>
+          {health.averageResponseTime}
+        </avgResponseTime></response>
 
       val p = new scala.xml.PrettyPrinter(80, 4)
       contentType = ContentTypes.`text/xml(UTF-8)`
@@ -62,7 +114,33 @@ trait ServiceRoutes {
     HttpEntity(contentType, response)
   }
 
+  def convertPriceResponse(price: PriceResponse, mediaTypeNegotiator: MediaTypeNegotiator) = {
+    var response = ""
+    var contentType: ContentType.NonBinary = ContentTypes.`application/json`
+    if (mediaTypeNegotiator.isAccepted(MediaTypes.`application/json`)) {
+      import MyJsonProtocol._
+      response = price.toJson.toString()
+
+    } else {
+      val pureXml =
+        <result>
+          {price.result}
+        </result>
+
+      val p = new scala.xml.PrettyPrinter(80, 4)
+      contentType = ContentTypes.`text/xml(UTF-8)`
+      response = p.format(pureXml)
+      pureXml
+
+
+    }
+
+    HttpEntity(contentType, response)
+  }
+
+
 }
+
 /**
   * @author ${user.name}
   */
@@ -74,12 +152,12 @@ object App extends App with ServiceRoutes {
 
 
   logger.info("Starting the service at http://localhost:8080")
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-
-
+  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
 
 }
 
 
-final case class HealthResponse(status: String)
+final case class HealthResponse(status: String, totalCalls: Long, averageResponseTime: Double)
+
+final case class PriceResponse(result: String)
--- src/test/scala/com/mrmonksy/daypricing/AppTest.scala
+++ src/test/scala/com/mrmonksy/daypricing/AppTest.scala
@@ -1,26 +1,68 @@
 package com.mrmonksy.daypricing
 
+import java.io.File
+
 import akka.http.scaladsl.model.{HttpHeader, StatusCode, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalatest.{FlatSpec, Matchers, WordSpec}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers, WordSpec}
 
-class AppTest extends WordSpec with Matchers with ScalatestRouteTest with ServiceRoutes  {
+class AppTest extends WordSpec with Matchers with ScalatestRouteTest with ServiceRoutes with BeforeAndAfter {
 
+  override def beforeAll(): Unit = {
+    val classLoader = getClass.getClassLoader
+    val file = new File(classLoader.getResource("application.json").getFile)
+
+    System.setProperty("config", file.getAbsolutePath)
+  }
   "Webservice::/healthcheck" should {
     "return the result for a health check in JSON (the default)" in {
       Get("/healthcheck") ~> route ~> check {
-        status should be (StatusCodes.OK)
-        responseAs[String] should be ("{\"status\":\"ok\"}")
+        status should be(StatusCodes.OK)
+        responseAs[String] should be("{\"status\":\"ok\",\"totalCalls\":1,\"averageResponseTime\":0.0}")
       }
     }
 
     "return the result for a health check in XML" in {
       Get("/healthcheck") ~> addHeader("Accept", "application/xml") ~> route ~> check {
-        status should be (StatusCodes.OK)
+        status should be(StatusCodes.OK)
         //TODO: Note this should have a doctype to have valid xml
-        responseAs[String] should be ("<status> ok </status>")
+        responseAs[String] should be("<response>\n    <status> ok </status>\n    <totalCalls> 1 </totalCalls>\n    <avgResponseTime> 0.0 </avgResponseTime>\n</response>")
       }
     }
   }
+
+  /* "Webservice::/findPrice/" should {
+    "Return a price for 2015-07-01T07:00:00Z to 2015-07-01T12:00:00Z in JSON" in {
+      Get("/findPrice/between/2015-07-01T07:00:00Z/2015-07-01T12:00:00Z") ~> route ~> check {
+
+        status should be(StatusCodes.OK)
+        responseAs[String] should be("""{"result": "1500"}"""")
+      }
+    }
+
+    "Return a price for 2015-07-01T07:00:00Z to 2015-07-01T12:00:00Z in XML" in {
+      Get("/findPrice/between/2015-07-01T07:00:00Z/2015-07-01T12:00:00Z") ~> addHeader("Accept", "application/xml") ~> route ~> check {
+        status should be(StatusCodes.OK)
+        responseAs[String] should be("<result> ok </result>")
+      }
+
+    }
+    "Return a price for 2015-07-04T07:00:00Z to 2015-07-04T12:00:00Z in JSON" in {
+      Get("/findPrice/between/2015-07-04T07:00:00Z/2015-07-04T12:00:00Z") ~> route ~> check {
+
+        status should be(StatusCodes.OK)
+        responseAs[String] should be("""{"result": "2000"}"""")
+      }
+    }
+    "Return a price for 2015-07-04T07:00:00Z to 2015-07-04T20:00:00Z in JSON" in {
+      Get("/findPrice/between/2015-07-04T07:00:00Z/2015-07-04T20:00:00Z") ~> route ~> check {
+
+        status should be(StatusCodes.OK)
+        responseAs[String] should be("""{"result": "unavailable"}"""")
+      }
+    }
+
+  }*/
+
 
 }
